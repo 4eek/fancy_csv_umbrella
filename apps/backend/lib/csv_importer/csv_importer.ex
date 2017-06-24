2e08e134abcaed389dd56d4825ae541f115aba28
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-24 19:22:48 -0300

    Start extracting a CsvRecordStream
    We will go to the next step: download stuff from the internet and
    feed our table. But our module is getting too complex, so before
    implementing that it's time to split up concerns.

--- apps/backend/lib/csv_importer/csv_importer.ex
+++ apps/backend/lib/csv_importer/csv_importer.ex
@@ -1,45 +1,51 @@
 defmodule CsvImporter.CsvImporter do
-  alias CsvImporter.{City, Repo}
+  defmodule CsvRecordStream do
+    alias CsvImporter.{City, Repo}
 
-  @headers ~w(name url)a
+    @headers ~w(name url)a
 
-  def call(file_handler) when is_pid(file_handler) do
-    stream = file_handler |> to_stream
-    headers = stream |> extract_headers
+    def create(file_handler) when is_pid(file_handler) do
+      stream = file_handler |> to_stream
+      headers = stream |> extract_headers
 
-    if valid?(headers) do
+      if valid?(headers) do
+        stream
+        |> Stream.map(&to_struct(&1, headers))
+        |> Enum.map(&Repo.insert(&1))
+
+        :ok
+      else
+        :invalid_csv
+      end
+    end
+
+    defp valid?(headers) do
+      Enum.count(headers) == Enum.count(@headers) &&
+      Enum.sort(headers) == Enum.sort(@headers)
+    end
+
+    defp to_stream(file_handler) do
+      file_handler
+      |> IO.stream(:line)
+      |> Stream.map(&String.strip/1)
+      |> Stream.map(&String.split(&1, ","))
+    end
+
+    defp extract_headers(stream) do
       stream
-      |> Stream.map(&to_struct(&1, headers))
-      |> Enum.map(&Repo.insert(&1))
+      |> Enum.fetch!(0)
+      |> Enum.map(&String.to_atom/1)
+    end
 
-      :ok
-    else
-      :invalid_csv
+    defp to_struct(line, headers) do
+      headers
+      |> Enum.zip(line)
+      |> Enum.into(%{})
+      |> (fn(contents) -> struct(City, contents) end).()
     end
   end
 
-  defp valid?(headers) do
-    Enum.count(headers) == Enum.count(@headers) &&
-    Enum.sort(headers) == Enum.sort(@headers)
-  end
-
-  defp to_stream(file_handler) do
-    file_handler
-    |> IO.stream(:line)
-    |> Stream.map(&String.strip/1)
-    |> Stream.map(&String.split(&1, ","))
-  end
-
-  defp extract_headers(stream) do
-    stream
-    |> Enum.fetch!(0)
-    |> Enum.map(&String.to_atom/1)
-  end
-
-  defp to_struct(line, headers) do
-    headers
-    |> Enum.zip(line)
-    |> Enum.into(%{})
-    |> (fn(contents) -> struct(City, contents) end).()
+  def call(file_handler) when is_pid(file_handler) do
+    CsvRecordStream.create(file_handler)
   end
 end
