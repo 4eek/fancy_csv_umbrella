eaa62c25dcc62c7ad38a6ff13fa06b17f70efde8
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-24 19:32:48 -0300

    Extract CsvRecordStream to another file

--- apps/backend/lib/csv_importer/csv_importer.ex
+++ apps/backend/lib/csv_importer/csv_importer.ex
@@ -1,50 +1,5 @@
 defmodule CsvImporter.CsvImporter do
-  defmodule CsvRecordStream do
-    alias CsvImporter.City
-
-    @headers ~w(name url)a
-
-    def create(file_handler) when is_pid(file_handler) do
-      stream = file_handler |> to_stream
-      headers = stream |> extract_headers
-
-      if valid?(headers) do
-        stream = stream
-        |> Stream.map(&to_struct(&1, headers))
-
-        {:ok, stream}
-      else
-        :invalid_csv
-      end
-    end
-
-    defp valid?(headers) do
-      Enum.count(headers) == Enum.count(@headers) &&
-      Enum.sort(headers) == Enum.sort(@headers)
-    end
-
-    defp to_stream(file_handler) do
-      file_handler
-      |> IO.stream(:line)
-      |> Stream.map(&String.strip/1)
-      |> Stream.map(&String.split(&1, ","))
-    end
-
-    defp extract_headers(stream) do
-      stream
-      |> Enum.fetch!(0)
-      |> Enum.map(&String.to_atom/1)
-    end
-
-    defp to_struct(line, headers) do
-      headers
-      |> Enum.zip(line)
-      |> Enum.into(%{})
-      |> (fn(contents) -> struct(City, contents) end).()
-    end
-  end
-
-  alias CsvImporter.Repo
+  alias CsvImporter.{CsvRecordStream, Repo}
 
   def call(file_handler) when is_pid(file_handler) do
     case CsvRecordStream.create(file_handler) do
