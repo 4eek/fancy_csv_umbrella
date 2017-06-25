5d0884059f5fbed54a3ce7eba45c18c08513bec1
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-25 11:39:32 -0300

    Extract a test helper

--- apps/backend/test/csv_importer/error_csv_builder_test.exs
+++ apps/backend/test/csv_importer/error_csv_builder_test.exs
@@ -2,12 +2,15 @@ defmodule CsvImporter.ErrorsCsvBuilderTest do
   use ExUnit.Case
   alias CsvImporter.{City, ErrorCsvBuilder}
 
+  defp read_file(handler), do: handler |> StringIO.contents |> contents
+  defp contents({_, contents}), do: contents
+
   test "writes the CSV header to a file" do
     {:ok, file_handler} = StringIO.open("")
 
     ErrorCsvBuilder.write_header(file_handler)
 
-    assert {_, "name,url,errors\n"} = StringIO.contents(file_handler)
+    assert "name,url,errors\n" = read_file(file_handler)
   end
 
   test "appends an invalid record to a file" do
@@ -17,6 +20,6 @@ defmodule CsvImporter.ErrorsCsvBuilderTest do
 
     ErrorCsvBuilder.write_line({:error, changeset}, record, file_handler)
 
-    assert {_, ",http://invalid.com,name can't be blank\n"} = StringIO.contents(file_handler)
+    assert ",http://invalid.com,name can't be blank\n" = read_file(file_handler)
   end
 end
