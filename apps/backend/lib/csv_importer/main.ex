6b1dab003211797a3efa2d7ceb79b490664d4f2e
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-25 23:49:24 -0300

    Refactor

--- apps/backend/lib/csv_importer/main.ex
+++ apps/backend/lib/csv_importer/main.ex
@@ -1,7 +1,7 @@
 defmodule CsvImporter.Main do
-  def import_file(input_file_handler, output_file_handler, on_update) do
-    alias CsvImporter.{CsvRecordStream, CsvImporter, ErrorCsvBuilder}
+  alias CsvImporter.{CsvRecordStream, CsvImporter, ErrorCsvBuilder}
 
+  def import_file(input_file_handler, output_file_handler, on_update) do
     ErrorCsvBuilder.write_header(output_file_handler)
 
     status = %{ok: 0, error: 0}
@@ -10,21 +10,24 @@ defmodule CsvImporter.Main do
     |> CsvRecordStream.create
     |> extract_ok_result
     |> CsvImporter.call
-    |> Stream.map(fn(record) ->
-      ErrorCsvBuilder.write_line(record, output_file_handler)
-      record
-    end)
-    |> Enum.reduce(status, fn
-      {{:ok, _}, _}, memo ->
-        memo = %{memo | ok: memo.ok + 1}
-        on_update.(memo)
-        memo
-      {{:error, _}, _}, memo ->
-        memo = %{memo | error: memo.error + 1}
-        on_update.(memo)
-        memo
-    end)
+    |> Stream.map(&write_output_line(&1, output_file_handler))
+    |> Enum.reduce(status, &sum_result(&1, &2, on_update))
   end
+
+  defp write_output_line(record, output_file_handler) do
+    record
+    |> ErrorCsvBuilder.write_line(output_file_handler)
+    |> (fn(_) -> record end).()
+  end
+
+  defp sum_result({{status, _}, _}, memo, callback) do
+    status
+    |> do_sum_result(memo)
+    |> callback.()
+  end
+
+  def do_sum_result(:ok, memo), do: %{memo | ok: memo.ok + 1}
+  def do_sum_result(:error, memo), do: %{memo | error: memo.error + 1}
 
   defp extract_ok_result({:ok, stream}), do: stream
 end
