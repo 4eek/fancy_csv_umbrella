e2fa0f3fe3df7127d2776a0e355dc4a9a1fd1d0c
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-29 07:14:30 -0300

    Refactor OutputCsv tests

--- apps/backend/test/csv_importer/output_csv_test.exs
+++ apps/backend/test/csv_importer/output_csv_test.exs
@@ -3,15 +3,16 @@ defmodule Backend.OutputCsvTest do
   alias Backend.{City, OutputCsv}
   import TestHelper, only: [read_stringio: 1]
 
-  test "creates a new output csv" do
+  setup do
     {:ok, device} = OutputCsv.new("", StringIO)
+    {:ok, device: device}
+  end
 
+  test "creates a new output csv", %{device: device} do
     assert "name,url,errors\n" = read_stringio(device)
   end
 
-  test "appends an invalid record" do
-    {:ok, device} = OutputCsv.new("", StringIO)
-
+  test "appends an invalid record", %{device: device} do
     record = %City{name: nil, url: "http://invalid.com"}
     changeset = City.changeset(record)
 
@@ -25,9 +26,7 @@ defmodule Backend.OutputCsvTest do
     assert expected_contents == read_stringio(device)
   end
 
-  test "does not append record when it is valid" do
-    {:ok, device} = OutputCsv.new("", StringIO)
-
+  test "does not append record when it is valid", %{device: device} do
     record = %City{name: "Town", url: "http://town.com"}
     changeset = City.changeset(record)
 
