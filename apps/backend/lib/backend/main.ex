7c8c2928b1a2e4bff6637d2c38f8801de97047db
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-06-28 22:02:53 -0300

    Refactor main module

--- apps/backend/lib/backend/main.ex
+++ apps/backend/lib/backend/main.ex
@@ -1,33 +1,48 @@
 defmodule Backend.Main do
-  alias Backend.{CsvRecordStream, CsvImporter, ErrorCsvBuilder}
+  alias Backend.{CsvRecordStream, ErrorCsvBuilder, Repo}
 
-  def import_file(input_file_handler, output_file_handler, on_update) do
-    ErrorCsvBuilder.write_header(output_file_handler)
+  def import_file(input_device, output_device, on_update) do
+    ErrorCsvBuilder.write_header(output_device)
 
-    status = %{ok: 0, error: 0}
-
-    input_file_handler
-    |> CsvRecordStream.create
-    |> extract_ok_result
-    |> CsvImporter.call
-    |> Stream.map(&write_output_line(&1, output_file_handler))
-    |> Enum.reduce(status, &sum_result(&1, &2, on_update))
+    input_device
+    |> create_csv_stream
+    |> import_records
+    |> write_output_csv(output_device)
+    |> sum_and_broadcast_stats(on_update)
   end
 
-  defp write_output_line(record, output_file_handler) do
-    record
-    |> ErrorCsvBuilder.write_line(output_file_handler)
-    |> (fn(_) -> record end).()
+  defp create_csv_stream(input_device) do
+    {:ok, stream } = CsvRecordStream.create(input_device)
+
+    stream
   end
 
-  defp sum_result({{status, _}, _}, memo, callback) do
-    memo = status |> do_sum_result(memo)
-    callback.(memo)
-    memo
+  defp import_records(stream) do
+    stream
+    |> Task.async_stream(__MODULE__, :import, [], max_concurrency: 10)
+    |> Stream.map(fn({:ok, tuple}) -> tuple end)
   end
 
-  def do_sum_result(:ok, memo), do: %{memo | ok: memo.ok + 1}
-  def do_sum_result(:error, memo), do: %{memo | error: memo.error + 1}
+  def import(record = %module{}) do
+    {record |> module.changeset |> Repo.insert, record}
+  end
 
-  defp extract_ok_result({:ok, stream}), do: stream
+  def write_output_csv(stream, output_device) do
+    stream
+    |> Stream.map(fn(tuple) ->
+      ErrorCsvBuilder.write_line(tuple, output_device)
+      tuple
+    end)
+  end
+
+  def sum_and_broadcast_stats(tuple, on_update) do
+    Enum.reduce(tuple, %{ok: 0, error: 0}, &sum_stats(&1, &2, on_update))
+  end
+
+  defp sum_stats({{result, _}, _}, stats, on_update) do
+    result |> sum_stats(stats) |> on_update.()
+  end
+
+  def sum_stats(:ok, stats), do: %{stats | ok: stats.ok + 1}
+  def sum_stats(:error, stats), do: %{stats | error: stats.error + 1}
 end
