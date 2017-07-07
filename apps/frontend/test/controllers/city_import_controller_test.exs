b8a0cb4cb492cea612571206a356560ba8b43351
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-07 08:06:57 -0300

    Improve integration test

--- apps/frontend/test/controllers/city_import_controller_test.exs
+++ apps/frontend/test/controllers/city_import_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule Frontend.CityImportControllerTest do
-  use Frontend.ConnCase
+  use Frontend.ConnCase, async: false
   import Phoenix.ChannelTest, only: [assert_broadcast: 2]
-  alias Frontend.BackgroundJob
-  alias Backend.City
+  alias Frontend.{BackgroundJob, CityCsvJob}
+  alias Backend.{City, Repo}
 
   test "GET /city_import/new", %{conn: conn} do
     conn = get conn, city_import_path(@endpoint, :new)
@@ -35,18 +35,25 @@ defmodule Frontend.CityImportControllerTest do
       }
     }
 
-    :ok = BackgroundJob.await_all
+    assert redirected_to(conn) == city_import_path(@endpoint, :index)
 
-    assert [
-      %{id: 1, data: %{ok: 3, error: 0, filename: "cities.csv"}}
-    ] = BackgroundJob.all
+    :ok = BackgroundJob.await_all
 
     assert_broadcast "change", %{error: 0, ok: 1, message: nil, output: nil}
     assert_broadcast "change", %{error: 0, ok: 2, message: nil, output: nil}
-    assert_broadcast "change", %{error: 0, ok: 3, message: nil, output: nil}
     assert_broadcast "change", %{error: 0, ok: 3, message: nil, output: "/files/cities" <> _rest}
 
-    assert redirected_to(conn) == city_import_path(@endpoint, :index)
-    assert ["Madrid", "Natal", "New York"] == Enum.map(City.all, &(&1.name))
+    assert [
+      %{id: 1,
+        data: %CityCsvJob{
+          ok: 3,
+          error: 0,
+          filename: "cities.csv",
+          output: "/files/cities" <> _rest
+        }
+      }
+    ] = BackgroundJob.all
+
+    assert 3 == Repo.aggregate(City, :count, :id)
   end
 end
