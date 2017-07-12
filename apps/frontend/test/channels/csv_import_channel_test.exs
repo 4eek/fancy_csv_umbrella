36e4c89a7f07be65601d6700204271cd5391f0a3
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-12 08:58:08 -0300

    Give socket a name

--- apps/frontend/test/channels/csv_import_channel_test.exs
+++ apps/frontend/test/channels/csv_import_channel_test.exs
@@ -6,7 +6,7 @@ defmodule Frontend.CsvImportChannelTest do
   test "sends initial jobs upon connecting" do
     Frontend.BackgroundJob.add %{j1: "j1"}, fn(_) -> nil end
 
-    {:ok, _, _socket} = socket("", %{})
+    {:ok, _, _socket} = socket("user:id", %{})
     |> subscribe_and_join(BackgroundJobChannel, "background_job")
 
     assert_push "initialize", %{jobs: [%{id: 1, data: %{j1: "j1"}}]}
