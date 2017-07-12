3b05fffe6688e1badbcf0a0ce279aa2fc9cd4373
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-12 08:31:04 -0300

    Cleanup files on test teardown

--- apps/frontend/test/support/background_job_cleanup.ex
+++ apps/frontend/test/support/background_job_cleanup.ex
@@ -3,11 +3,25 @@ defmodule Frontend.BackgroundJobCleanup do
     quote do
       setup do
         on_exit fn ->
-          Frontend.BackgroundJob.delete_all
+          Enum.each Frontend.BackgroundJob.all, fn(job) ->
+            if Map.has_key?(job.data, :output) do
+              Frontend.BackgroundJobCleanup.delete_output_files(job)
+            end
+          end
         end
+
+        Frontend.BackgroundJob.delete_all
 
         :ok
       end
+    end
+  end
+
+  def delete_output_files(%{data: %{output: output}}) do
+    files = Path.wildcard("**/#{Path.basename(output)}")
+
+    Enum.each files, fn(file) ->
+      :ok = File.rm(file)
     end
   end
 end
