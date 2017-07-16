f493aa535b38f9321531f6a4ec93a53419e4bf37
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-07-15 23:04:46 -0300

    Use delegate for Frontend.JobRunner

--- apps/frontend/lib/frontend/job_runner.ex
+++ apps/frontend/lib/frontend/job_runner.ex
@@ -1,27 +1,11 @@
 defmodule Frontend.JobRunner do
   @name :job_runner
 
-  def start_link do
-    JobRunner.start_link name: @name
-  end
+  def start_link, do: JobRunner.start_link name: @name
 
-  def add(pid \\ @name, initial_state, callback) do
-    JobRunner.add(pid, initial_state, callback)
-  end
-
-  def all(pid \\ @name) do
-    JobRunner.all(pid)
-  end
-
-  def await_all(pid \\ @name) do
-    JobRunner.await_all(pid)
-  end
-
-  def update(pid \\ @name, options) do
-    JobRunner.update(pid, options)
-  end
-
-  def delete_all(pid \\ @name) do
-    JobRunner.delete_all(pid)
-  end
+  defdelegate add(pid \\ @name, initial_state, callback), to: JobRunner
+  defdelegate all(pid \\ @name), to: JobRunner
+  defdelegate await_all(pid \\ @name), to: JobRunner
+  defdelegate update(pid \\ @name, options), to: JobRunner
+  defdelegate delete_all(pid \\ @name), to: JobRunner
 end
